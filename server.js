28b43eed48b49fda83d3c3a9818eb72002282f58
KTKin <143994319+KTKin@users.noreply.github.com>
2023-11-20 22:40:42 +0800

    Update server.js

--- server.js
+++ server.js
@@ -3,44 +3,80 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.set('view engine', 'ejs');
+
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const session = require('cookie-session');
+app.use(session({
+	name : 'login-session',
+	keys : ['login passport']
+}));
 
-app.set('view engine', 'ejs');
 
-//set mongodb
-const MongoClient = require('mongodb').MongoClient;
+//mongodb
+const {MongoClient} = require('mongodb');
 const dbUrl = 'mongodb+srv://ktk10566:ktk10566@cluster0.mgewehr.mongodb.net/?retryWrites=true&w=majority';
-const dbName = 'test';
-
 const client = new MongoClient(dbUrl);
-
-client.connect(err => {
-	ssert.equal(null,err);
-	console.log('Connected to database');
-	const db = client.db(dbName);
-	client.close();
-});
-
 
 
 //routes
-//default route
+//---default route
 app.get('/', (req, res) => {
-	res.status(200).redirect('/login');
+	if (req.session.authenticated == true) {
+		res.redirect('/main');
+	} else {
+		res.redirect('/login');
+	}
 });
 
-//login
+//---login
 app.get('/login', (req, res) => {
-	res.render('login',{});
+	res.status(200).render('login',{});
 });
 
-app.post('/login', (req, res) => {
-	var userInput = req.body.username;
-	var pwInput = req.body.password;
+app.post('/login', async(req, res) => {
+	try{
+		await client.connect();
+		console.log('Connected to database...');
+		
+		var query = { userID : req.body.username };
+		
+		//find user
+		var result = await client.db('project').collection('students').findOne(query);
+		if (result != null) {
+			console.log('User found...');
+			
+			//match password
+			if (result.userPW == req.body.password) {
+				console.log('Login successful...');
+				req.session.authenticated = true;
+				req.session.username = result.fName + ' ' + result.lName;
+			} else {
+				console.log('Wrong password...');
+			}
+		} else {
+			console.log('User not found...');
+		}
+	} finally {
+		await client.close();
+		console.log('Disconnect from database...')
+		res.redirect('/');
+	}
+});
+
+//---logout
+app.get('/logout', (req, res) => {
+	req.session = null;
+	console.log('Logout...')
+	res.redirect('/');
+});
+
+//---main
+app.get('/main', (req, res) => {
+	res.status(200).render('main',{});
 });
 
 
